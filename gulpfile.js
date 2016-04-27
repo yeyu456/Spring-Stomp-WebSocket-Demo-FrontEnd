480090f7b7d1e9e3ba914001eb1ada8f63510189
yuye <yeyu456@gmail.com>
2016-04-27 18:29:12 +0800

    update eslint

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ require("babel-polyfill");
 
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 const path = require('path');
@@ -11,15 +12,18 @@ const eslint = require('gulp-eslint');
 
 const scriptDir = path.join(__dirname, 'browser', 'scripts');
 
-gulp.task('check', function _check(cb) {
+gulp.task('check', function _check() {
     return gulp.src(path.join(scriptDir, '*.js'))
-            .pipe(eslint('./'))
+            .pipe(eslint('./eslintrc'))
             .pipe(eslint.format())
             .pipe(eslint.failAfterError());
 });
 
-gulp.task('babel')
+gulp.task('babel', ['check'], function _es6(cb) {
 
-gulp.task('default', ['check'], function _default(cb) {
+    cb();
+});
+
+gulp.task('default', ['babel'], function _default(cb) {
     cb();
 });
